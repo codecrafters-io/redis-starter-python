d34898a6bed26efd0a4319b639a9a15ee52d3fbf
Paul Kuruvilla <rohitpaulk@gmail.com>
2020-02-13 16:34:38 +0530

    Syncing Python repository with redis-challenge-repo-syncer
    Created by https://github.com/rohitpaulk/redis-challenge-repo-syncer.

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ def main():
     # Uncomment this to pass the first stage
     #
     # import socket
-    # s = socket.create_server(("localhost", 6379), reuse_port=True)
-    # s.listen()  # Wait for a connection
+    # socket.create_server(("localhost", 6379), reuse_port=True)
 
 
 if __name__ == "__main__":
